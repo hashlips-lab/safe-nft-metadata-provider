ac653fe311596c8112aa1c2f82d690b078facde0
Marco Lipparini <1532277+liarco@users.noreply.github.com>
2022-01-13 15:06:39 +0100

    Removing the TotalSupplyController and replacing it with a dedicated CLI command (#4)

---
+++ src/Command/TotalSupplyCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Safe NFT Metadata Provider package.
+ *
+ * (c) Marco Lipparini <developer@liarco.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Contract\TotalSupplyProviderInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * @author Marco Lipparini <developer@liarco.net>
+ */
+#[AsCommand(
+    name: 'nft:total-supply',
+    description: 'Returns the current total supply',
+)]
+class TotalSupplyCommand extends Command
+{
+    public function __construct(
+        private readonly TotalSupplyProviderInterface $totalSupplyProvider,
+        string $name = null,
+    ) {
+        parent::__construct($name);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $symfonyStyle = new SymfonyStyle($input, $output);
+
+        $symfonyStyle->success('Current total supply: '.$this->totalSupplyProvider->getTotalSupply());
+
+        return Command::SUCCESS;
+    }
+}
