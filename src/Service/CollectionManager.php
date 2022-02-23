f184838293639be6d152d7aff6ed7a9b3ead9eb7
Marco Lipparini <developer@liarco.net>
2022-02-23 14:11:20 +0100

    Fixing CI

--- src/Service/CollectionManager.php
+++ src/Service/CollectionManager.php
@@ -166,7 +166,10 @@ final class CollectionManager
     {
         $this->collectionFilesystemDriver->storeExportedMetadata(
             $tokenId,
-            $this->getMetadata($tokenId, $uriPrefix.'/'.$tokenId.'.'.$this->collectionFilesystemDriver->getAssetsExtension()),
+            $this->getMetadata(
+                $tokenId,
+                $uriPrefix.'/'.$tokenId.'.'.$this->collectionFilesystemDriver->getAssetsExtension(),
+            ),
         );
     }
 
